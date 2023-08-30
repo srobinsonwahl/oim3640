803a22bc2c2af6923f92b7e522c4f1b8e2db77b4
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-08-30 17:01:33 -0400

    Exercise 2 solved

---
+++ session02/calc.py
@@ -0,0 +1,11 @@
+# Q1 - How many seconds are there in 42 minutes 42 seconds? 
+print('There are', 42 * 60 + 42, 'seconds in 42 minutes and 42 seconds.')
+# Answer: 2562
+
+#  Q2 - How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile?
+print('There are ', 10 / 1.61, 'miles in 10 kilometers.')
+#  Answer: 6.2111
+
+# Q3 - If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace in time per mile and average speed in miles per hour?
+print('If you run a 10k race in 42 minutes and 42 seconds, your average pace is', 2562 / 60 / 6.2111, 'minutes per mile and', 6.2111 / 2562 * 60 * 60, 'miles per hour.')
+#  Answer: 6.8747 mpm and 8.7275 mph
