855db9d1fcbd5fd400dfd1e69b4dd0c6c0aa01f3
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-09-20 16:03:36 -0400

    quiz0 answer

---
+++ quiz/quiz0.py
@@ -0,0 +1,26 @@
+"""
+Question 1:
+
+Write a function that prompts the user for an amount in USD (float), converts it to the equivalent amount in Euro (EUR), and prints the result.
+
+Currency Conversion Rates:
+
+1 USD = 0.94 EUR
+
+Notice:
+1. Please write pseudo-code before coding the function.
+2. Your function should include docstring.
+3. Write your own test code, i.e. the code that calls the function.
+"""
+
+# Pseudocode:
+# Prompt the user for an amount in USD thats a float
+# Take the input, convert it to a float, and convert it to EUR value
+# Print the converted float
+
+def convertusdeur():
+    amount = float(input('Please enter an amount in USD:'))
+    convert = amount * 0.94
+    print(f'This is the amount of Euro, the USD amount you entered is worth: {convert:.02f}')
+
+convertusdeur()
