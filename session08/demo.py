2ee3ad893d1b1e180d5c2f6825b3a993dcd60d04
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-09-25 16:32:08 -0400

    session 8 class work

---
+++ session08/demo.py
@@ -0,0 +1,21 @@
+# def f(a, b, c):
+#     return a * 10 + b * 10 + c
+
+# res = f(3,4,5)
+
+# # print(res)
+
+# res_2 = f(c=5, a=3, b=4)
+
+# # print(res_2)
+
+# res_3 = f(3,c=4,b=6)
+
+# print(res_3)
+
+# # print('Hi', 'Larry', sep='###'
+
+# x = 1
+# y = 2
+# x, y = y, x
+# print(x, y)
---
+++ session08/iteration_demo.py
@@ -0,0 +1,32 @@
+# i = 0
+
+# while i < 3: # as long as this condition is True
+#     print('Hi')
+#     i += 1
+
+# for i in range(3):
+#     print('Hi')
+
+# calculate the sum of integers from 0 - 10:
+
+# total = 0
+
+# for i in range(10+1):
+#     print(f'Iteration {i}:')
+#     print(f'    before adding {i}, the current total is {total}')
+#     total += i
+#     print(f'    after adding {i}, the total beomes {total}')
+#     print()
+
+
+# print(total)
+
+# repeat asking user to enter password until it is correct
+
+while True:
+    password = input('Enter your password: ')
+    if password == '123456':
+        print('Welcome!')
+        break
+    else:
+        print('Wrong! Please enter again')
