416a10d30259a1e199b92b4290c27bcd1484f216
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-11-09 10:32:12 -0500

    class 21

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect,render_template, request
 from weather import get_temperature
 
 app = Flask(__name__)
 
 
 # If the website domain is www.xyz.com, http://www.xyz.com/ will trigger the function below
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def index():
+#     return "<p>Hello, World!</p>"
 
 
 # If the website domain is www.xyz.com, http://www.xyz.com/hello and http://www.xyz.com/hello/<name> will trigger the function below
+@app.route('/')
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     if name:
         return render_template('hello.html', username = name)
-    return "<h1>Hello, guest!</h1>"
+    return render_template('hello.html')
 
 
 # Create another route like '/square/<num>', so the web app will display the square of the number
@@ -32,16 +33,45 @@ def square(num=None):
 def page_not_found(e):
     return render_template('404.html')
 
-@app.route('/weather/', methods=['GET', 'POST'])
-def get_weather():
-    if request.method == 'POST':
-        city_name = request.form('city')
-        temp = get_temperature(city_name)
+# @app.route('/weather/', methods=['GET', 'POST'])
+# def get_weather():
+#     if request.method == 'POST':
+#         city_name = request.form['city']
+#         temp = get_temperature(city_name)
 
-        return f'The current temperature of the city {city_name} is {temp}.'
-    else:
-        return render_template('weather-form.html')
+#         return f'The current temperature of the city {city_name} is {temp}.'
+#     else:
+#         return render_template('weather-form.html')
 
+@app.get('/weather/')
+def weather_get():
+    return render_template('weather-form.html')
+
+@app.post('/weather/')
+def weather_post():
+    city_name = request.form['city']
+    temp = get_temperature(city_name)
+    return f'The current temperature of the city {city_name} is {temp}.'
+
+STUDENTS = {}
+COURSES = ['Python', 'Web', 'Cybersecurity', 'Mobile App']
+
+@app.route('/students/')
+def show_students():
+    return render_template('students.html', students = STUDENTS, courses = COURSES)
+
+@app.get('/register/')
+def register_get():
+    return render_template('/register-form.html')
+
+@app.post('/register/')
+def register_post():
+    name = request.form['name']
+    course = request.form['course']
+    if course not in COURSES:
+        return 'Hi hacker, you cannot register this course!'
+    STUDENTS[name] = course
+    return redirect('/students/')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- hello_flask/weather.py
+++ hello_flask/weather.py
@@ -9,7 +9,7 @@ def get_temperature(city):
     APIKEY = '7cb24f95fbf2512e936a2834ab896b2b'
     city = 'Wellesley'
     country_code = 'us'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}&units=imperial'
     with urllib.request.urlopen(url) as f:
         response_text = f.read().decode('utf-8')
         response_data = json.loads(response_text)
